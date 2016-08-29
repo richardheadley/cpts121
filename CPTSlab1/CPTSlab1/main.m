529b79739aa331426e33cedf6fe4bfffd86c2c6d
richardheadley <Dickbook@richard-headleys-MacBook-Pro.local>
2016-08-29 15:18:09 -0700

    Created lab 1 document and assignment

---
+++ CPTSlab1/CPTSlab1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CPTSlab1
+//
+//  Created by richard headley on 8/26/16.
+//  Copyright © 2016 richard headley. All rights reserved.
+//
+
+// Author: Richard Headley
+// Course: Cpt S 121
+// Lab: 01
+
+#import <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    printf("hello world\n");
+    return 0;
+}
+
+
+
